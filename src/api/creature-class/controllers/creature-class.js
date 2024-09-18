a3480a6859b0cdaab0f62dc0ea4f970e4b2aa0e1
lynx-r <lynx-r@users.noreply.github.com>
2024-09-18 20:16:17 +0000

    deploy: 472918d7c4640a7b320888beedf9ebe509fc36f1

---
+++ src/api/creature-class/controllers/creature-class.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * creature-class controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::creature-class.creature-class');
---
+++ src/api/familia/controllers/familia.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * familia controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::familia.familia');
